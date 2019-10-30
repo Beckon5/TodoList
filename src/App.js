4a8cb7e30d613079d31b203e71f2b898c96af5e0
Beckon5 <mmTaraNmm@gmail.com>
2019-10-30 16:41:19 +0200

    Create design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-main">
+      <p className="todo-main__title">
+        To do list...
+      </p>
+      <div className="todo-main__input-field">
+        <button className="check-all">
+          All
+        </button>
+        <input className="input" type="text" placeholder="What to do?"/>
+      </div>
+      <div className="todo-main__list">
+        <input className="check" type="checkbox"/>
+        <p className="text">Hi, im one of your todo </p>
+      </div>
+      <div className="todo-main__list">
+        <input className="check" type="checkbox"/>
+        <p className="text">Hi, im one of your todo </p>
+      </div>
+      <div className="todo-main__list">
+        <input className="check" type="checkbox"/>
+        <p className="text">Hi, im one of your todo </p>
+      </div>
     </div>
   );
 }
